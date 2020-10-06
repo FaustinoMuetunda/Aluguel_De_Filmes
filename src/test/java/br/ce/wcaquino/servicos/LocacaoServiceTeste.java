51e9e82e3cdd214f876dc912f7c15706c3979d86
FaustinoPaulo <faustinomuetunda@gmail.com>
2020-10-06 17:30:39 +0100

    Separei a classe de teste do metodo principal. No src/teste, defini a classe de teste, usando o mesmo endereco do pacote da classe para facilitar no acesso dos metodos e variaveis da classe

---
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTeste.java
@@ -0,0 +1,47 @@
+package br.ce.wcaquino.servicos;
+
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.servicos.LocacaoService;
+import br.ce.wcaquino.utils.DataUtils;
+
+public class LocacaoServiceTeste {
+	@Test
+	public void teste() {
+		//Cenario
+		
+		LocacaoService service=new LocacaoService();
+		Usuario usuario=new Usuario("Usuario 1");
+		Filme filme=new Filme("Filme 1",2,5.0);
+		
+		//Accao
+		Locacao locacao=service.alugarFilme(usuario, filme);
+		
+		//Verificacao
+		
+		System.out.println(locacao.getValor());
+		System.out.println(locacao.getDataLocacao());
+		System.out.println(locacao.getDataRetorno());
+		
+		//Verificar se os atributos estao conforme o esperado
+		System.out.println();
+		
+		
+		Assert.assertTrue(locacao.getValor() ==5.0);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		
+		
+		
+		
+		
+	}
+
+}
